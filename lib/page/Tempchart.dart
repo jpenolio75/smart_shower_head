2cfe3eb92578821650a1569355b71470f7f50b24
Angelo <98061732+AngeloNol@users.noreply.github.com>
2023-11-14 13:03:15 -0800

    Add files via upload

--- lib/page/Tempchart.dart
+++ lib/page/Tempchart.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'dart:async';
-import 'dart:math' as math;
+//import 'dart:math';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/scheduler.dart';
+
+class chart extends StatelessWidget {
+  const chart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Tempchart());
+  }
+}
 
 class Tempchart extends StatefulWidget {
+  const Tempchart({Key? key}) : super(key: key);
+
   @override
-  _Tempchart createState() => _Tempchart();
+  _TempchartState createState() => _TempchartState();
 }
+
+class _TempchartState extends State<Tempchart> {
+  List<_ChartData> chartData = <_ChartData>[];
+
+  @override
+  void initState() {
+    getDataFromFireStore().then((results) {
+      SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
+        setState(() {});
+      });
+    });
+    super.initState();
+  }
+
+  Future<void> getDataFromFireStore() async {
+    var snapShotsValue = await FirebaseFirestore.instance
+        .collection("Temperature/AppData")
+        .get();
+    List<_ChartData> list = snapShotsValue.docs
+        .map((e) => _ChartData(
+            x: DateTime.fromMillisecondsSinceEpoch(
+                e.data()['x'].millisecondsSinceEpoch),
+            y: e.data()['y']))
+        .toList();
+    setState(() {
+      chartData = list;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _showChart();
+  }
+
+  Widget _showChart() {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SfCartesianChart(
+          tooltipBehavior: TooltipBehavior(enable: true),
+          primaryXAxis: CategoryAxis(),
+          series: <LineSeries<_ChartData, DateTime>>[
+            LineSeries<_ChartData, DateTime>(
+                dataSource: chartData,
+                xValueMapper: (_ChartData data, _) => data.x,
+                yValueMapper: (_ChartData data, _) => data.y)
+          ]),
+    );
+  }
+}
+
+class _ChartData {
+  _ChartData({this.x, this.y});
+  final DateTime? x;
+  final double? y; //temp
+}
+
+/*class Tempchart extends StatefulWidget {
+  @override
+  _Tempchartstate createState() => _Tempchartstate();
+}
+
+
 
 class _Tempchart extends State<Tempchart> {
   late List<TempData> chartdata;
@@ -30,7 +107,7 @@ class _Tempchart extends State<Tempchart> {
               _chartSeriesController = controller;
             },
             dataSource: chartdata,
-            color: const Color.fromRGBO(192, 108, 132, 1),
+            color: Color.fromARGB(255, 245, 11, 11),
             xValueMapper: (TempData temp, _) => temp.time,
             yValueMapper: (TempData temp, _) => temp.temp,
           ) //LineSeries
@@ -73,4 +150,4 @@ class TempData {
   TempData(this.time, this.temp);
   final int time;
   final double temp;
-}
+}*/
